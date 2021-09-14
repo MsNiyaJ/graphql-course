8a7bb7054bb63ea05d0f4b21d041efc7e37683cf
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-09-14 09:26:13 -0400

    Setup a server

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-import myCurrentLocation, { getGreeting, message, name } from "./myModule";
-import addFunction, { subtract } from "./math";
+import { GraphQLServer } from "graphql-yoga";
 
-// You can also grab the default export by typing the following
-// import myCurrentLocation from "./myModule";
+// 5 Scalar Types: String, Boolean, Int, Float, ID      (A type that stores a single value)
 
-console.log('Calling math.js');
-console.log(addFunction(5, 5))      //10
-console.log(subtract(10, 5))        //5
-console.log('');
-console.log('Calling myModule.js');
-console.log(message);                 // Some message from myModule.js
-console.log(name);                    // Shaniya Malcolm
-console.log(myCurrentLocation);       // New York
-console.log(getGreeting(`Camilla`));  // Welcome to the course Camilla;  
+// Type definition schema
+const typeDefs = `
+    type Query{
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        hello(){
+            return 'This is my first query!'
+        }, 
+        name(){
+            return 'Shaniya Malcolm'
+        },
+        location(){
+            return 'New York'
+        },
+        bio(){
+            return 'I am a black, beautiful, and intelligent'
+        }
+    }
+}
+
+// Start the servers
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('The server is running');
+})
