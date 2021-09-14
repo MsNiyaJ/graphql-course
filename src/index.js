e7899d8958d15eeaf3e6b967c8095c44fb0d80a1
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-09-14 10:01:36 -0400

    Created custom types

--- src/index.js
+++ src/index.js
@@ -1,36 +1,50 @@
 import { GraphQLServer } from "graphql-yoga";
 
 // 5 Scalar Types: String, Boolean, Int, Float, ID      (A type that stores a single value)
-// (!) - Indicates this query is manditory
+// (!) - Indicates this query is mandatory
+// All type definitions start with a capital letter
 
 // Type definition schema
 const typeDefs = `
     type Query{
+       me: User! 
+       post: Post!  
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
         title: String!
-        price: Float!
-        releaseYear: Int
-        rating: Float
-        inStock: Boolean!    
+        body: String!
+        published: Boolean!
     }
 `
 
 // Resolvers
 const resolvers = {
     Query: {
-        title(){
-            return 'Hunger Games'
-        }, 
-        price(){
-            return 29.99
+        me(){
+            return {
+                id: '123098',
+                name: 'Priscilla',
+                email: 'PZilla@email.com',
+                age: null
+            }
         },
-        releaseYear(){
-            return null
-        },
-        rating(){
-            return 4.3
-        },
-        inStock(){
-            return true
+
+        post(){
+            return {
+                id: '11111',
+                title: 'My thoughts',
+                body: 'Life is a rollercoaster',
+                published: false
+            }
         }
     }
 }
